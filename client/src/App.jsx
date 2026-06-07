2cc3ea2f39c4af2a23b065014e19560607956641
Vinesh0409 <vineshaggarwal2007@outlook.com>
2026-06-07 20:37:57 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
